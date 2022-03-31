6de6aff3999dde981a6be436ba25bc24be0dfc8f
alexis-fondard <alexis.fondard@etu.umontpellier.fr>
2022-03-31 19:12:57 +0200

    test

--- server.js
+++ server.js
@@ -1,17 +1,28 @@
-const express = require("express");
-const dotEnv = require('dotenv')
-const path = require('path')
-const port = process.env.PORT || 8080;
+// const express = require("express");
+// const dotEnv = require('dotenv')
+// const path = require('path')
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// dotEnv.config({
+//     path: path.join(__dirname, ".env")
+// });
+
+// app.use(express.static(__dirname + "/dist/"));
+// app.get(/.*/, function (req, res) {
+//     res.sendFile(__dirname + "/dist/index.html");
+// });
+// app.listen(port);
+
+// console.log("Server started...");
+
+const express = require('express');
 const app = express();
 
-dotEnv.config({
-    path: path.join(__dirname, ".env")
-});
+// Serve all the files in '/dist' directory
+app.use(express.static('dist'));
 
-app.use(express.static(__dirname + "/dist/"));
-app.get(/.*/, function (req, res) {
-    res.sendFile(__dirname + "/dist/index.html");
-});
-app.listen(port);
 
-console.log("Server started...");
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
